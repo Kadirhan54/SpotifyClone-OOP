555fa6ce898636be0bbbfcc9433cbdf8e28118d9
Kadirhan54 <kadirhansaglam5454@gmail.com>
2023-09-25 18:39:55 +0300

    Add project files.

---
+++ SpotifyClone/Entitites/Playlist.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpotifyClone.Entitites
+{
+    public class Playlist
+    {
+
+        public Guid Id { get; set;} = Guid.NewGuid();
+
+        private List<Song> SongList { get; set; } = new List<Song>();
+
+        public Random random = new Random();
+
+        public Playlist(Song song) 
+        {
+            AddSong(song);
+        } 
+
+
+        public void AddSong(Song song) 
+        {
+            if (song != null && SongList.Contains(song) == false)
+            {
+                SongList.Add(song);
+            }
+        }
+
+        public string GetSongs()
+        {
+            return string.Join("\n", SongList.Select(x=> $"{x.Title} - {x.Composer}"));
+        }
+
+        public void ShuffleSongs()
+        {
+            int count = SongList.Count;
+
+            while (count > 0)
+            {
+                count--;
+
+                int randomIndex = random.Next(0, count);
+
+                Song song = SongList[count];
+
+                SongList[count] = SongList[randomIndex];
+
+                SongList[randomIndex] = song;
+
+            }
+        }
+    }
+}
---
+++ SpotifyClone/Program.cs
@@ -0,0 +1,21 @@
+﻿
+using SpotifyClone.Entitites;
+using SpotifyClone.Servies;
+
+Song song1 = new Song("1","2","3","4");
+Song song2 = new Song("5","6","7","8");
+Song song3 = new Song("9","10","11","12");
+
+Playlist playlist = new Playlist(song1);
+playlist.AddSong(song2);
+playlist.AddSong(song3);
+
+playlist.ShuffleSongs();
+
+NotepadService service= new NotepadService();
+
+service.PlaylistToNotepad(playlist);
+
+
+
+
